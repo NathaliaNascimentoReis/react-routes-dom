85e67b7b7a5e2b8b50130eea2730436e34cd638a
Nath <nathalia.n.reis@aluno.senai.br>
2026-05-09 19:07:37 -0300

    feat: finaliza atividade

--- react-routes/src/App.jsx
+++ react-routes/src/App.jsx
@@ -1,10 +1,33 @@
 import './App.module.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import NavBar from './components/NavBar/NavBar.jsx';
+import Footer from './components/Footer/Footer.jsx';
+import Home from './pages/Home/Home.jsx';
+import Contatos from './pages/Contatos/Contatos.jsx';
+import Sobre from './pages/Sobre/Sobre.jsx';
+import Servicos from './pages/Servicos/Servicos.jsx';
+import NotFound from './pages/NotFound/NotFound.jsx';
+import Extra from './pages/Extra/Extra.jsx';
 
 function App() {
     return (
         <>
+            <BrowserRouter>
+                <NavBar></NavBar>
+                <main style={{ padding: '20px' }}>
+                    <Routes>
+                        <Route path="/" element={<Home></Home>}></Route>
+                        <Route path="/sobre" element={<Sobre></Sobre>}></Route>
+                        <Route path="/servicos" element={<Servicos></Servicos>}></Route>
+                        <Route path="/equipe" element={<Extra></Extra>}></Route>
+                        <Route path="/contatos" element={<Contatos></Contatos>}></Route>
+                        <Route path="/*" element={<NotFound></NotFound>}></Route>
+                    </Routes>
+                </main>
+                <Footer></Footer>
+            </BrowserRouter>
         </>
-    )
+    );
 }
 
 export default App;
--- react-routes/src/components/NavBar/NavBar.jsx
+++ react-routes/src/components/NavBar/NavBar.jsx
@@ -1,16 +1,16 @@
-// Editaremos aqui
+import { Link } from 'react-router-dom';
 import styles from './Navbar.module.css';
 
 function Navbar() {
     return (
         <nav className={styles.navbar}>
-            <div className={styles.logo}>LOGO</div>
+            <div className={styles.logo}>React</div>
             <ul className={styles.links}>
-                {/* Editaremos aqui */}
-                {/* Editaremos aqui */}
-                {/* Editaremos aqui */}
-                {/* Editaremos aqui */}
-                {/* Editaremos aqui */}
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/sobre">Sobre</Link></li>
+                <li><Link to="/servicos">Serviços</Link></li>
+                <li><Link to="/equipe">Equipe</Link></li>
+                <li><Link to="/contato">Contato</Link></li>
             </ul>
         </nav>
     );
